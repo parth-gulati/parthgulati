bb3360e97cf9f21944e9204c3ec5a72280d11f3b
Parth Gulati <gulati.parth98@gmail.com>
2020-10-14 18:07:11 +0530

    changed number of github repos

--- src/ResumeData.js
+++ src/ResumeData.js
@@ -27,7 +27,7 @@ let factsData = {
   description:
     "I strive to effectively learn and master new languages and implement them in new projects.",
   cd1: "Projects on Github",
-  c1: 17,
+  c1: 19,
   cd2: "Certifications",
   c2: 9,
   cd3: "Programming Languages",
